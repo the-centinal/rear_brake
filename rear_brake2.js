84d6cba5c9d306593f4618beb184a1289789f2ac
the-centinal <78435208+the-centinal@users.noreply.github.com>
2022-11-01 00:49:51 +0530

    Update rear_brake2.js

--- rear_brake2.js
+++ rear_brake2.js
@@ -8,30 +8,31 @@ const plugin = ({ widgets, simulator, vehicle }) => {
             <div class="speed-chart" style="height:300px !important; width: 300px !important;">
                 <div>
                 <canvas id="myChart" style="height:300px !important; width: 300px !important;"></canvas>
-                <h1>hello</h1>
-    
+               
                 </div>
             </div>`)
 
-                var x_co = [0, 5, 10, 15, 20, 25, 30];
-            var y_co = [0, 15, 30, 47, 50, 45, 30];
+
+          
+            var x_co = [0, 5, 10, 15, 20, 25, 30];
+        
+            var y_co = [0, 15, 30, 47, 50, 45, 30]
+          
 
 
-            loadScript(box.window, "https://cdn.jsdelivr.net/npm/chart.js")
-            const ctx =  div.querySelector("#myChart");
-            const myChart  = new Chart(ctx, {
+            const data = {
+                labels: x_co,
+                datasets: [{
+                    label: 'Rear Brake Temperature',
+                    backgroundColor: '#34d2eb',
+                    borderColor: '#34d2eb',
+                    data: y_co
+                }]
+            };
 
+            const config = {
                 type: 'line',
-                data : {
-                    labels: x_co,
-                    datasets: [{
-                        label: 'Rear Brake Temperature',
-                        backgroundColor: '#34d2eb',
-                        borderColor: '#34d2eb',
-                        data: y_co
-                    }]
-                },
-
+                data: data,
                 options: {
                     scales: {
                         y: {
@@ -49,19 +50,24 @@ const plugin = ({ widgets, simulator, vehicle }) => {
                         }
                     }
                 }
+            };
+            loadScript(box.window, "https://cdn.jsdelivr.net/npm/chart.js")
 
-            })
+            const myChart = new Chart(
+                
+                div.querySelector("#myChart"),
+                config
+            );
             box.injectNode(div)
-        
-            
+            console.log("all good");
         }
+
+
 
     })
 }
 
 export default plugin
-
-
 
 
 
