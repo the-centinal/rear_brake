0d1c5c8217ac6a13f9c2680b663fd5e306bd6906
the-centinal <78435208+the-centinal@users.noreply.github.com>
2022-11-01 15:28:43 +0530

    Update rear_brake2.js

--- rear_brake2.js
+++ rear_brake2.js
@@ -18,8 +18,8 @@ const plugin = ({ widgets, simulator, vehicle }) => {
                   </div>
               </div>`);
 
-         console.log(div.querySelectorAll("#myChart").length);
-
+         if(div.querySelectorAll("#myChart").length > 0 ){
+           setInterval(function() {
             var x_co = [0, 5, 10, 15, 20, 25, 30];
 
             var y_co = [0, 15, 30, 47, 50, 45, 30]
@@ -56,7 +56,47 @@ const plugin = ({ widgets, simulator, vehicle }) => {
                         }
                     }
                 }
-            };
+            };}, 1000)
+         };
+         
+
+//             var x_co = [0, 5, 10, 15, 20, 25, 30];
+
+//             var y_co = [0, 15, 30, 47, 50, 45, 30]
+
+
+
+//             const data = {
+//                 labels: x_co,
+//                 datasets: [{
+//                     label: 'Rear Brake Temperature',
+//                     backgroundColor: '#34d2eb',
+//                     borderColor: '#34d2eb',
+//                     data: y_co
+//                 }]
+//             };
+
+//             const config = {
+//                 type: 'line',
+//                 data: data,
+//                 options: {
+//                     scales: {
+//                         y: {
+//                             title: {
+//                                 display: true,
+//                                 text: 'Temp (in °C)'
+//                             }
+//                         },
+//                         x: {
+//                             title: {
+//                                 display: true,
+//                                 text: 'Time (in mins.)'
+
+//                             }
+//                         }
+//                     }
+//                 }
+//             };
             loadScript(box.window, "https://cdn.jsdelivr.net/npm/chart.js")
           
           const myChart = new Chart(
