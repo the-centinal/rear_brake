2d70a01d8b6f3122aec92883230426fbab0b935f
the-centinal <78435208+the-centinal@users.noreply.github.com>
2022-11-01 13:35:17 +0530

    Update rear_brake2.js

--- rear_brake2.js
+++ rear_brake2.js
@@ -1,10 +1,11 @@
+
 import loadScript from "./loadscript.js";
 const plugin = ({ widgets, simulator, vehicle }) => {
-const div = document.createElement("div");
+    const div = document.createElement("div");
 
 
-            widgets.register("rearbrake",function(box,msg){
-              div.innerHTML = (`
+    widgets.register("rearbrake", function (box, msg) {
+        div.innerHTML = (`
               <div class="speed-chart" style="height:280px !important; width: 280px !important;">
                   <div>
                   <canvas id="myChart" style="height:280px !important; width: 280px !important;"></canvas>
@@ -12,61 +13,61 @@ const div = document.createElement("div");
               </div>`);
 
 
-            var x_co = [0, 5, 10, 15, 20, 25, 30];
+        var x_co = [0, 5, 10, 15, 20, 25, 30];
 
-            var y_co = [0, 15, 30, 47, 50, 45, 30]
+        var y_co = [0, 15, 30, 47, 50, 45, 30]
 
 
 
-            const data = {
-                labels: x_co,
-                datasets: [{
-                    label: 'Rear Brake Temperature',
-                    backgroundColor: '#34d2eb',
-                    borderColor: '#34d2eb',
-                    data: y_co
-                }]
-            };
+        const data = {
+            labels: x_co,
+            datasets: [{
+                label: 'Rear Brake Temperature',
+                backgroundColor: '#34d2eb',
+                borderColor: '#34d2eb',
+                data: y_co
+            }]
+        };
 
-            const config = {
-                type: 'line',
-                data: data,
-                options: {
-                    scales: {
-                        y: {
-                            title: {
-                                display: true,
-                                text: 'Temp (in °C)'
-                            }
-                        },
-                        x: {
-                            title: {
-                                display: true,
-                                text: 'Time (in mins.)'
+        const config = {
+            type: 'line',
+            data: data,
+            options: {
+                scales: {
+                    y: {
+                        title: {
+                            display: true,
+                            text: 'Temp (in °C)'
+                        }
+                    },
+                    x: {
+                        title: {
+                            display: true,
+                            text: 'Time (in mins.)'
 
-                            }
                         }
                     }
                 }
-            };
-            loadScript(box.window, "https://cdn.jsdelivr.net/npm/chart.js")
+            }
+        };
+        loadScript(box.window, "https://cdn.jsdelivr.net/npm/chart.js")
 
-            const myChart = new Chart(
+        const myChart = new Chart(
 
-                div.querySelector("#myChart"),
-                config
-            );
-            box.injectNode(div);
-            console.log("all good");
-            return () => {
-                        boxGlobal = null
-                        // Deactivation function for clearing intervals or such.
-                    }
-            };
+            div.querySelector("#myChart"),
+            config
+        );
+        box.injectNode(div);
+        console.log("all good");
+        return () => {
+            boxGlobal = null
+            // Deactivation function for clearing intervals or such.
+        }
+    });
 
-      });
+};
 
-        export default plugin
+export default plugin
 
 
 
